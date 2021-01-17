93666214e766466379dd1050bfe49ec43de4df0c
Nataliia333 <n.o.prykhodko@gmail.com>
2021-01-17 23:32:31 +0200

    шаблоны

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,8 @@
+function fetchCountries(searchQuery) {
+    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+    return (fetch(url)
+    .then(res => res.json())
+  );
+};
+
+export default fetchCountries;
